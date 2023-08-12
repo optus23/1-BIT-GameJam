124026ecf04de78b2485298ae237cc665b487f5d
Lluís Moreu <lluis.moreu@gmail.com>
2023-08-13 01:01:06 +0200

    material colour changes with light

--- Assets/Lighthouse.cs
+++ Assets/Lighthouse.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class Lighthouse : MonoBehaviour
 {
-    public float maxPValue = 3;
-    public float minPValue = 0;
-    public float maxDValue = 1;
-    public float minDValue = 0;
-    public float intenP = 0; // the total
-    float changePerSecond; // modify the total, every second
-    public float timeToChange = 8; // the total time myValue will take to go from max to min
-    bool add = true;
+    public float maxPValue = 3f;
+    public float minPValue = 0f;
+    public float maxDValue = 1f;
+    public float minDValue = 0f;
+    float intenP = 0f; // the total
     float intenD = 0.0f;
+    float changePerSecond; // modify the total, every second
+    public float timeToChange = 8f; // the total time myValue will take to go from max to min
+    bool add = true;
+
     Light pointLight;
     Light directionalLight;
+
+    public Renderer[] renderer_assets;
+    Color colour;
+    float c = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +30,8 @@ public class Lighthouse : MonoBehaviour
         directionalLight.intensity = intenD;
 
         changePerSecond = (minPValue - maxPValue) / timeToChange;
+
+        colour = new Color(c, c, c);
     }
 
     // Update is called once per frame
@@ -37,14 +44,24 @@ public class Lighthouse : MonoBehaviour
         {
             intenP = Mathf.Clamp(intenP - changePerSecond * Time.deltaTime, minPValue, maxPValue);
             intenD = Mathf.Clamp(intenD - changePerSecond * Time.deltaTime, minDValue, maxDValue);
+            c = Mathf.Clamp(c - changePerSecond * Time.deltaTime, 0.0f, 1.0f);
         }
         else
         {
             intenP = Mathf.Clamp(intenP + changePerSecond * Time.deltaTime, minPValue, maxPValue);
             intenD = Mathf.Clamp(intenD + changePerSecond * Time.deltaTime, minDValue, maxDValue);
+            c = Mathf.Clamp(c + changePerSecond * Time.deltaTime, 0.0f, 1.0f);
         }
 
         pointLight.intensity = intenP;
         directionalLight.intensity = intenD;
+        colour.r = c;
+        colour.g = c;
+        colour.b = c;
+
+        for (int i = 0; i < renderer_assets.Length; i++)
+        {
+            renderer_assets[i].material.color = colour;
+        }
     }
 }
