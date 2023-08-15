37587c8bdebd5c9f41e0f9b15ed64ed391bb7187
optus23 <optus23@gmail.com>
2023-08-15 22:13:46 +0200

    Sailor body complete. But, BUG generated :-1:

--- Assets/Scripts/Paint.cs
+++ Assets/Scripts/Paint.cs
@@ -6,30 +6,26 @@ using UnityEngine;
 
 public class Paint : MonoBehaviour
 {
-    public Texture2D tex;
     public LayerMask IgnoreLayerMask;
     public Camera MainCamera;
-    public GameObject Plane;
     [SerializeField]
     private Transform m_Cursor;
     
     void Start()
     {
         //tex = new Texture2D(64,64);
-
-        Plane.GetComponent<Renderer>().sharedMaterial.mainTexture = tex;
     }
 
     void Update()
     {
-        if (tex != null)
-        {
             Ray ray = MainCamera.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit, Mathf.Infinity, ~IgnoreLayerMask))
             {
                 if (Input.GetButton("Fire1"))
                 {
+                    Texture2D tex = hit.transform.gameObject.GetComponent < Renderer >().sharedMaterial.mainTexture as Texture2D;
+
                     // Find the u,v coordinate of the Texture
                     Vector2 uv;
                     uv.x = (hit.point.x - hit.collider.bounds.min.x) / hit.collider.bounds.size.x;
@@ -79,33 +75,11 @@ public class Paint : MonoBehaviour
                     }
                     
                     
-                    
-                    
-                    
-                    
                     tex.Apply();
                 } 
-               
-                  
-                   
-                    
                 // Set Cursor Position
                 m_Cursor.position = new Vector3( hit.point.x, hit.point.y, hit.point.z);
-
-
             }
-            //if (Input.GetButton("Fire2"))
-            //{
-            //    for (int i = 0; i < 128; i++)
-            //    {e
-            //        for (int j = 0; j < 128; j++)
-            //        {
-            //            tex.SetPixel(i, j, Color.white);
-            //        }
-            //    }
-            //    tex.Apply();
-            //}
-        }
     }
     
 }
