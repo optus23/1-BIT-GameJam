9a2d25940888172a674c911a268222ac60b71005
optus23 <optus23@gmail.com>
2023-08-15 12:25:55 +0200

    Added zoom effect feature

--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -8,33 +8,42 @@ using Object = UnityEngine.Object;
 
 public class MouseController : MonoBehaviour
 {
-
     [SerializeField]
-    private RawImage m_RawImage;
+    private Transform m_ZoomCamera;
+    
+    [SerializeField]
+    private Transform m_QuadRenderTexture;
+    
     [SerializeField]
     private Camera m_Camera;
     private bool m_enableZoomEffect;
+    
+    [SerializeField]
     private Vector3 m_MousePosition;
+    
+    [SerializeField]
     private Vector3 m_WorldPosition;
 
+    [SerializeField]
+    private Vector3 m_CameraOffset;
+    
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            
-
             m_enableZoomEffect = !m_enableZoomEffect;
-            m_RawImage.enabled = m_enableZoomEffect;
+            m_QuadRenderTexture.gameObject.SetActive(m_enableZoomEffect);
         }
 
         if ( m_enableZoomEffect )
         {
             m_MousePosition = Input.mousePosition;
+            m_MousePosition.z = m_Camera.nearClipPlane + 1;
+            
             m_WorldPosition = m_Camera.ScreenToWorldPoint( m_MousePosition );
-
-            m_WorldPosition = new Vector3( m_WorldPosition.x, m_WorldPosition.y, m_RawImage.transform.position.z );
-            m_RawImage.transform.position = m_WorldPosition;
+            
+            m_ZoomCamera.position = new Vector3(m_WorldPosition.x * m_CameraOffset.x, m_WorldPosition.y * m_CameraOffset.y, m_ZoomCamera.position.z);
+            m_QuadRenderTexture.position = m_WorldPosition;
         }
-        
     }
 }
