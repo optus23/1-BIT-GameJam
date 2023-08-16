6dc6829acaf28ecdfea68bdd868a074965d83a60
Lluís Moreu <lluis.moreu@gmail.com>
2023-08-16 23:43:27 +0200

    startBtn prefab and startgame.cs

---
+++ Assets/StartGame.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+    public Camera MainCamera;
+    GameObject button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Ray ray = MainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                if (hit.transform.gameObject.name == "StartBtn")
+                {
+                    button = hit.transform.gameObject;
+                    button.SetActive(false);
+                }
+            }
+        }
+    }
+}
