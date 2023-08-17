ae4de78b44ecde60f6be65d1a621a2b2f2f7f1df
Lluís Moreu <lluis.moreu@gmail.com>
2023-08-17 22:28:28 +0200

    camerashake script(add this to maincamera)
    StartShake method has timer and magnitude as input variables

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ////test
+        //if (Input.GetKeyDown(KeyCode.E))
+        //{
+        //    StartShake(1f, 0.4f);
+        //}
+    }
+
+    public void StartShake(float shakeTime, float shakeMagnitude)
+    {
+        StartCoroutine(Shake(shakeTime, shakeMagnitude));
+    }
+
+    public IEnumerator Shake(float duration, float magnitude)
+    {
+        Vector3 orignalPosition = transform.position;
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            transform.position = new Vector3(x, y, -10f);
+            elapsed += Time.deltaTime;
+            yield return 0;
+        }
+        transform.position = orignalPosition;
+    }
+}
